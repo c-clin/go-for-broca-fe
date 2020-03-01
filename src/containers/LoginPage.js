36cbe437f5e7ad829eea8d26b5456a4d426b5de2
Catherine Lin <catherine.lin.93@gmail.com>
2020-02-29 23:59:57 -0800

    add google onSignIn

--- src/containers/LoginPage.js
+++ src/containers/LoginPage.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
 class LoginPage extends Component {
+  onSignIn = googleUser => {
+    let id_token = googleUser.getAuthResponse().id_token;
+    console.log(googleUser);
+  };
   render() {
     return (
       <div className='LoginPage'>
-        <div class='g-signin2' data-onsuccess='onSignIn'></div>
+        <div class='g-signin2' data-onsuccess={this.onSignIn}></div>
       </div>
     );
   }

45f7e09dedf8d26c651d274ab67d1d805533a122
Catherine Lin <catherine.lin.93@gmail.com>
2020-03-05 21:44:31 -0800

    changed var name

--- src/axios-api.js
+++ src/axios-api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import keys from './config/keys';
 
 const authInstance = axios.create({
-  baseURL: `${keys.HOST}/api`,
+  baseURL: `${keys.BACKEND_HOST}/api`,
   headers: {
     accept: 'application/json'
   }
--- src/config/prod.js
+++ src/config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-  HOST: process.env.HOST
+  HOST: process.env.BACKEND_HOST
 };
